c15efecd991be515f6eb6b7f40e4803d1093a831
Stefano <stefano2890@gmail.com>
2020-04-24 18:49:06 +0200

    creata funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,23 +1,45 @@
 // È palindroma?
 
 // creare una funzione per verificare se una parola è palindroma e stampare di conseguenza un messaggio appropriato;
-// chiedo all' utente di inserire una parola;
-var parola = prompt('Inserisci una parola:')
-console.log('La parola inserita è: ' + parola);
 
-// estrapolo ogni singola lettera e la inserisco in un array;
+// creo la funzione parolaPalindroma;
+function parolaPalindroma(parola) {
+
+// estrapolo ogni singola lettera della parola e la inserisco in un array;
 var lettera = parola.split('')
+
 // inverto l'ordine degli elementi che compongo l'array, ovvero le singole lettere;
 var reverse = lettera.reverse()
+
 // ricompogo la parola invertita sotto forma di stringa;
 var reverse_ricomposta = reverse.join('')
 
 // verfico che la parola inserita dall'utente sia palidroma, ovvero uguale alla stessa parola invertita;
 if (parola == reverse_ricomposta ) {
-    // se è vero stampo la frase che mi conferma che la parola è palidroma;
-    console.log('La parola è palindroma');
+    return true;
+    // se è vero ritorno il risultato true;
 
 } else {
-    // altrimenti stampo la frase che dice che la parola inserita non è palindroma, e quindi che la parola è diversa dalla sua versione scritta a lettere invertite;
+    return false;
+    // altrimenti se la parola è diversa dalla sua versione scritta a lettere invertite, non è palindroma e quindi ritorno il risultato false;
+}
+}
+// fine della funzione parolaPalindroma;
+
+// ora utilizzo la funzione creata;
+
+// chiedo all'utente di inserire una parola e la stampo;
+var parola_utente = prompt('Inserisci una parola:')
+console.log('La parola inserita è: ' + parola_utente);
+
+// creo una variabile per comparare il risultato della funzione;
+var palindroma = parolaPalindroma(parola_utente)
+
+// condizione per effettuare la verifica;
+if (palindroma == true) {
+    // se dalla funzione mi ritorna true allora stampo il seguente messaggio;
+    console.log('La parola è palindroma');
+} else {
+    // altrimenti se dalla fuzione mi ritorna false stampo il seguente messaggio;
     console.log('La parola non è palindroma');
 }
